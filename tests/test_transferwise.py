24e86f1d4cbd48a8be29619ddb3daa98ae03b140
Wígny <wignybora@gmail.com>
2025-10-30 11:58:24 -0400

    test: setup snapshot test

--- tests/test_transferwise.py
+++ tests/test_transferwise.py
@@ -5,7 +5,7 @@ from ofxstatement.ui import UI
 from ofxstatement_wise.wise import TransferwisePlugin
 
 
-def test_transferwise() -> None:
+def test_transferwise(snapshot) -> None:
     config = {"currency": "USD", "account": "TW1"}
     plugin = TransferwisePlugin(UI(), config)
     here = os.path.dirname(__file__)
@@ -14,9 +14,4 @@ def test_transferwise() -> None:
     parser = plugin.get_parser(sample_filename)
     statement = parser.parse()
 
-    assert statement is not None
-
-    assert len(statement.lines) == 5
-    # all ids are unique
-    assert len(set(ln.id for ln in statement.lines)) == 5
-    assert all(ln.amount for ln in statement.lines)
+    assert statement == snapshot
